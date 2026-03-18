0a26c270619936cebec394600cb8606ba411d42f
Vikash Gupta <153454474+Vikashak47@users.noreply.github.com>
2026-03-18 17:03:20 +0530

    Update messageRouter.js

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -3,9 +3,9 @@ const { authToken } = require('../middleware/authMiddleware');
 const { getUsersForSidebar, getMessages, sendMessage } = require('../controllers/messageControllers');
 const router = express.Router();
 
-router.get("/users", getUsersForSidebar);
-router.get("/:id", getMessages);
+router.get("/users",authToken, getUsersForSidebar);
+router.get("/:id",authToken, getMessages);
 
-router.post("/send/:id", sendMessage);
+router.post("/send/:id",authToken, sendMessage);
 
 module.exports = router;
